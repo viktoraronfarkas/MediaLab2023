02321d3bc7ba245859f0fdd43ca240bed219c7d4
Viktor Farkas <viktoraron.farkas@gmail.com>
2023-04-26 15:13:58 +0200

    add community guidelines view

---
+++ src/views/CommunityGuidelines.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { View, Text } from 'react-native';
+
+export default function CommunityGuidelines() {
+  <SafeAreaView>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 28, textAlign: 'center' }}>
+        Here will be the community guidelines
+      </Text>
+    </View>
+  </SafeAreaView>;
+}
