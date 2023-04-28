d8d3657cb7760ff849c1c8784134b146d36f39f0
lauraSup <laura.m.suppan@gmail.com>
2023-04-28 12:32:16 +0200

    The color constants are added in the constants folder

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import theme from '../constants/myTheme';
 
 export default function Home() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ fontSize: 28, textAlign: 'center' }}>
+        <Text
+          style={{
+            fontSize: 28,
+            textAlign: 'center',
+            color: theme.colors.primary,
+          }}
+        >
           Welcome to the first FH Social App
         </Text>
       </View>
---
+++ src/constants/myTheme.js
@@ -0,0 +1,36 @@
+import { MD3LightTheme } from 'react-native-paper';
+
+const theme = {
+  ...MD3LightTheme,
+
+  colors: {
+    ...MD3LightTheme.colors,
+
+    primary: 'rgb(243, 79, 52)',
+    onPrimary: 'rgb(255, 255, 255)',
+    secondary: 'rgb(0, 80, 151)',
+    error: 'rgb(243, 173, 52)',
+    onError: 'rgb(18, 18, 18)',
+    success: 'rgb(52, 243, 77)',
+    onSuccess: 'rgb(18, 18, 18)',
+    info: 'rgb(52, 218, 243)',
+    onInfo: 'rgb(18, 18, 18)',
+    attention: 'rgb(243, 52, 122)',
+    onAttention: 'rgb(18, 18, 18)',
+    backgroundWhite: 'rgb(255, 255, 255)',
+    onBackgroundWhite: 'rgb(18, 18, 18)',
+    backgroundSand: 'rgb(245, 241, 236)',
+    onBackgroundSand: 'rgb(18, 18, 18)',
+    backgroundCamel: 'rgb(223, 218, 211)',
+    onBackgroundCamel: 'rgb(18, 18, 18)',
+    onBackgroundCamelSandColor: 'rgb(243, 79, 52)',
+    neutralsWhite: 'rgb(255, 255, 255)',
+    neutralsGrey300: 'rgb(224, 224, 224)',
+    neutralsGrey500: 'rgb(158, 158, 158)',
+    neutralsGrey800: 'rgb(98, 98, 98)',
+    neutralsDark: 'rgb(50, 50, 50)',
+    neutralsBlack: 'rgb(0, 0, 0)',
+  },
+};
+
+export default theme;
