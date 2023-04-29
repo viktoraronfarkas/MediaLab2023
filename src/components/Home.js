77481b9cd817e1861dfaa4b09a5e5e895e21db49
cc201013 <bedo.faruk13@gmail.com>
2023-04-29 16:15:57 +0200

    WIP

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,37 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View } from 'react-native';
+import { Provider as PaperProvider, Chip, Button } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import theme from '../constants/myTheme';
 
 export default function Home() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text
-          style={{
-            fontSize: 28,
-            textAlign: 'center',
-            color: theme.colors.primary,
-          }}
+    <PaperProvider theme={theme}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View
+          style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
         >
-          Welcome to the first FH Social App
-        </Text>
-      </View>
-    </SafeAreaView>
+          <Text
+            style={{
+              fontSize: 28,
+              textAlign: 'center',
+              color: theme.colors.primary,
+            }}
+          >
+            Welcome to the first FH Social App
+          </Text>
+          <Chip icon="information" onPress={() => console.log('Pressed')}>
+            Example Chip
+          </Chip>
+          <Button
+            icon="camera"
+            mode="contained"
+            onPress={() => console.log('Pressed')}
+          >
+            Press me
+          </Button>
+        </View>
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
--- src/constants/myTheme.js
+++ src/constants/myTheme.js
@@ -5,10 +5,13 @@ const theme = {
 
   colors: {
     ...MD3LightTheme.colors,
-
     primary: 'rgb(243, 79, 52)',
     onPrimary: 'rgb(255, 255, 255)',
+    primaryContainer: 'rgb(240, 219, 255)',
+    onPrimaryContainer: 'rgb(44, 0, 81)',
     secondary: 'rgb(0, 80, 151)',
+    secondaryContainer: 'rgb(255, 221, 246)',
+    onSecondaryContainer: 'rgb(33, 24, 42)',
     error: 'rgb(243, 173, 52)',
     onError: 'rgb(18, 18, 18)',
     success: 'rgb(52, 243, 77)',
@@ -17,6 +20,8 @@ const theme = {
     onInfo: 'rgb(18, 18, 18)',
     attention: 'rgb(243, 52, 122)',
     onAttention: 'rgb(18, 18, 18)',
+    background: 'rgb(255, 255, 255)',
+    surfaceVariant: 'rgb(255, 255, 255)',
     backgroundWhite: 'rgb(255, 255, 255)',
     onBackgroundWhite: 'rgb(18, 18, 18)',
     backgroundSand: 'rgb(245, 241, 236)',
