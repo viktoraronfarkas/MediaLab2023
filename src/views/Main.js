34b4775747c5b72363e027e1e31d4b72f062ccf0
cc201013 <bedo.faruk13@gmail.com>
2023-05-02 21:54:20 +0200

    Done

--- src/views/Main.js
+++ src/views/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import BottomNav from '../components/BottomNavigation';
+import { HomeSvg, MessageSvg, ProfileSvg, SearchSvg } from '../components/SVG';
 
 export default function Main() {
   return (
@@ -10,7 +11,16 @@ export default function Main() {
       style={{ flex: 1, backgroundColor: 'red' }}
     >
       <View style={{ flex: 1, backgroundColor: 'red' }}>
-        <BottomNav />
+        <BottomNav
+          HomeSvgFocused={<HomeSvg color="#F34F34" />}
+          HomeSvgUnfocused={<HomeSvg color="#000" />}
+          SearchSvgFocused={<SearchSvg color="#F34F34" />}
+          SearchSvgUnfocused={<SearchSvg color="#000" />}
+          MessageSvgFocused={<MessageSvg color="#F34F34" />}
+          MessageSvgUnfocused={<MessageSvg color="#000" />}
+          ProfileSvgFocused={<ProfileSvg color="#F34F34" />}
+          ProfileSvgUnfocused={<ProfileSvg color="#000" />}
+        />
       </View>
     </SafeAreaView>
   );
