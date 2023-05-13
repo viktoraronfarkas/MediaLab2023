287379bd9c7c1894f2af530d4cee5068ad7d4320
Omar Fares <omar.fares2626@gmail.com>
2023-05-14 01:46:50 +0200

    ML2-104 : Create the main group all screen

--- src/components/Buttons/SubGroupsFilter.js
+++ src/components/Buttons/SubGroupsFilter.js
@@ -1,26 +1,133 @@
-import * as React from 'react';
-import { SegmentedButtons } from 'react-native-paper';
+import React, { useState } from 'react';
+import { View, TouchableOpacity, Text } from 'react-native';
+import { theme } from '../../constants/myTheme';
 
-function SubGroupsFilter() {
-  const [value, setValue] = React.useState('');
+function SubGroupsFilter({
+  firstFilterLabel,
+  secondFilterLabel,
+  thirdFilterLabel,
+}) {
+  const [selected, setSelected] = useState('all');
 
   return (
-    <SegmentedButtons
-      style={{ width: '100%' }}
-      value={value}
-      onValueChange={setValue}
-      buttons={[
-        {
-          value: 'all',
-          label: 'all',
-        },
-        {
-          value: 'joined',
-          label: 'joined',
-        },
-        { value: 'unjoined', label: 'unjoined' },
-      ]}
-    />
+    <View
+      style={{
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        borderRadius: 12,
+        justifyContent: 'center',
+        alignContent: 'center',
+        width: '80%',
+        padding: '2%',
+      }}
+    >
+      <View
+        style={{
+          marginLeft: '3%',
+          marginRight: '3%',
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            backgroundColor:
+              selected === firstFilterLabel
+                ? theme.colors.primary
+                : theme.colors.backgroundCamel,
+            borderColor:
+              selected === firstFilterLabel
+                ? theme.colors.primary
+                : theme.colors.backgroundCamel,
+            justifyContent: 'center', // align text vertically
+            alignItems: 'center', // align text horizontally
+            width: 88,
+            height: 30,
+            borderRadius: 12,
+          }}
+          onPress={() => setSelected(firstFilterLabel)}
+        >
+          <Text
+            style={{
+              textAlign: 'center',
+              color:
+                selected === firstFilterLabel
+                  ? '#fff'
+                  : theme.colors.neutralsGrey800,
+            }}
+          >
+            all
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity
+          style={{
+            backgroundColor:
+              selected === secondFilterLabel
+                ? theme.colors.primary
+                : theme.colors.backgroundCamel,
+            borderColor:
+              selected === secondFilterLabel
+                ? theme.colors.primary
+                : theme.colors.backgroundCamel,
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 88,
+            height: 30,
+            borderRadius: 12,
+          }}
+          onPress={() => setSelected(secondFilterLabel)}
+        >
+          <Text
+            style={{
+              textAlign: 'center',
+              color:
+                selected === secondFilterLabel
+                  ? '#fff'
+                  : theme.colors.neutralsGrey800,
+            }}
+          >
+            joined
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          marginLeft: '3%',
+          marginRight: '3%',
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            backgroundColor:
+              selected === thirdFilterLabel
+                ? theme.colors.primary
+                : theme.colors.backgroundCamel,
+            borderColor:
+              selected === thirdFilterLabel
+                ? theme.colors.primary
+                : theme.colors.backgroundCamel,
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 88,
+            height: 30,
+            borderRadius: 12,
+          }}
+          onPress={() => setSelected(thirdFilterLabel)}
+        >
+          <Text
+            style={{
+              textAlign: 'center',
+              color:
+                selected === thirdFilterLabel
+                  ? '#fff'
+                  : theme.colors.neutralsGrey800,
+            }}
+          >
+            unjoined
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
 
--- src/views/AddSubgroup.js
+++ src/views/AddSubgroup.js
@@ -1,81 +1,76 @@
 import React from 'react';
 import { View, Text, SafeAreaView, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import InputField from '../components/Items/InputField';
 import { styles, theme } from '../constants/myTheme';
 import OrangeButton from '../components/Buttons/OrangeButton';
 import CaptionScribbleHeading from '../components/Texts/CaptionScribbleHeading';
 import UploadIcon from '../../assets/Icons/upload-icon.png';
-import GlitterImage from '../../assets/Images/glitter-image.png'
+import GlitterImage from '../../assets/Images/glitter-image.png';
 import BackButton from '../components/Buttons/BackButton';
 
-
-
-
 function AddSubgroup() {
+  const navigation = useNavigation();
+  const handlePress = () => {
+    navigation.goBack(null);
+    // dispatch(actionExample(''));
+  };
   return (
     <SafeAreaView style={{ backgroundColor: theme.colors.backgroundSand }}>
-        <View style={{ margin: 20 }}>
-            <View style={{ marginBottom:10, marginTop:10 }}>
-                <BackButton
-                    text="back"
-                    onPress={() => {
-                    navigation.goBack(null), dispatch(actionExample(''));
-                    }}
-                />
-            </View>
-            <CaptionScribbleHeading 
-                subHeading="Give it all to me" 
-                title="Enter all the infos for your Subgroup"
-                scribbleSubHeadingImage={GlitterImage} 
-                scribbleStyle={{ width: 35, height: 35}}
-            />
-
-            <View style={{ marginTop: 40 }}>
-                <View style={{ marginBottom: 5 }}>
-                    <Text style={styles.subtitle2}>Name your Subgroup:</Text>
-                </View>
-                <InputField labelText="Name" />
-                <View style={{ marginLeft: 40 }}>
-                    <Text style={styles.navLabel}>Limit to 15 Characters</Text>
-                </View>            
-            </View>
-
-            <View style={{ marginTop: 20 }}>
-                <View style={{ marginBottom: 5 }}>
-                    <Text style={styles.subtitle2}>Write a great caption:</Text>
-                </View>
-                <InputField labelText="Caption" />
-                <View style={{ marginLeft: 40 }}>
-                    <Text style={styles.navLabel}>Limit to 15 Characters</Text>
-                </View>
-            </View>
-            
-            <View style={{ marginTop: 20 }}>
-                <Text style={styles.subtitle2}>Add an Image:</Text>
-                <View style={{ marginLeft: 20, marginTop: 15 }}>
-                    <Image source={UploadIcon} style={{width: 60, height: 60}}/>
-                </View>
-            </View>
-
-            <View style={{ marginTop: 20 }}>
-                <View style={{ marginBottom: 5 }}>
-                    <Text style={styles.subtitle2}>Write a small Introduction:</Text>
-                </View>
-                <InputField labelText="Introduction" />
-            </View>
-
-            <View style={{ marginTop: 40 }}>
-                <OrangeButton text="Post me!"  styleButton={{  alignSelf: 'center', width: '100%' }}  />
-            </View>
-            
+      <View style={{ margin: 20 }}>
+        <View style={{ marginBottom: 10, marginTop: 10 }}>
+          <BackButton text="back" onPress={handlePress} />
         </View>
-    </SafeAreaView>
+        <CaptionScribbleHeading
+          subHeading="Give it all to me"
+          title="Enter all the infos for your Subgroup"
+          scribbleSubHeadingImage={GlitterImage}
+          scribbleStyle={{ width: 35, height: 35 }}
+        />
 
+        <View style={{ marginTop: 40 }}>
+          <View style={{ marginBottom: 5 }}>
+            <Text style={styles.subtitle2}>Name your Subgroup:</Text>
+          </View>
+          <InputField labelText="Name" />
+          <View style={{ marginLeft: 40 }}>
+            <Text style={styles.navLabel}>Limit to 15 Characters</Text>
+          </View>
+        </View>
+
+        <View style={{ marginTop: 20 }}>
+          <View style={{ marginBottom: 5 }}>
+            <Text style={styles.subtitle2}>Write a great caption:</Text>
+          </View>
+          <InputField labelText="Caption" />
+          <View style={{ marginLeft: 40 }}>
+            <Text style={styles.navLabel}>Limit to 15 Characters</Text>
+          </View>
+        </View>
+
+        <View style={{ marginTop: 20 }}>
+          <Text style={styles.subtitle2}>Add an Image:</Text>
+          <View style={{ marginLeft: 20, marginTop: 15 }}>
+            <Image source={UploadIcon} style={{ width: 60, height: 60 }} />
+          </View>
+        </View>
+
+        <View style={{ marginTop: 20 }}>
+          <View style={{ marginBottom: 5 }}>
+            <Text style={styles.subtitle2}>Write a small Introduction:</Text>
+          </View>
+          <InputField labelText="Introduction" />
+        </View>
+
+        <View style={{ marginTop: 40 }}>
+          <OrangeButton
+            text="Post me!"
+            styleButton={{ alignSelf: 'center', width: '100%' }}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
-
-
-
 export default AddSubgroup;
-
