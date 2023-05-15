1111a9136e89ba74540f0930d6b887f250229396
Lenzia207 <lenzia207@gmail.com>
2023-05-15 12:29:22 +0200

    adjusted comments

--- src/views/Login_Registration/ScreenNavigation.js
+++ src/views/Login_Registration/ScreenNavigation.js
@@ -1,26 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
-// import { StyleSheet } from 'react-native';
 import { theme } from '../../constants/myTheme';
 
 import LoginScreen from './Login/LoginScreen';
 import RegistrationScreen from './Registration/RegistrationScreen';
 
-// const style = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: theme.colors.backgroundSand,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   text: {
-//     fontSize: 42,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     color: theme.colors.primary,
-//   },
-// });
 export default function ScreenNavigation() {
   const Stack = createStackNavigator();
 
