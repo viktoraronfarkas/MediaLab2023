41c50c0f7e59928037c8bd22b7b1ec5e938e58df
Viktor Farkas <viktoraron.farkas@gmail.com>
2023-05-23 22:04:32 +0200

    add subgroup APIs

--- src/routes/controllers/subgroupController.js
+++ src/routes/controllers/subgroupController.js
@@ -45,6 +45,65 @@ exports.getPostsBySubgroupId = (req, res) => {
   });
 };
 
+exports.createPost = (req, res) => {
+  // eslint-disable-next-line consistent-return
+  upload(req, res, (uploadErr) => {
+    // Handle file upload errors
+    if (uploadErr instanceof multer.MulterError) {
+      return res.status(500).json({ message: 'Error uploading file' });
+    }
+    if (uploadErr) {
+      console.error('Error uploading file:', uploadErr);
+      return res
+        .status(500)
+        .json({ message: 'Error uploading file', error: uploadErr.message });
+    }
+
+    // Retrieve user registration data from the request body
+    const { groupId, userId, text } = req.body;
+
+    // Validate required fields
+    if (!groupId) {
+      return res.status(400).json({ message: 'Group ID not provided' });
+    }
+    if (!userId) {
+      return res.status(400).json({ message: 'User ID not provided' });
+    }
+    if (!text) {
+      return res.status(400).json({ message: 'Text not provided' });
+    }
+
+    // Access the file buffer instead of the file path
+    const titleImage = req.file ? req.file.buffer : null;
+
+    // Connect to the database pool
+    // eslint-disable-next-line consistent-return
+    pool.getConnection((dbErr, connection) => {
+      if (dbErr) {
+        console.error('Error getting MySQL connection:', dbErr);
+        return res.status(500).json({ message: 'Failed to connect to MySQL' });
+      }
+
+      connection.query(
+        'INSERT INTO posts (group_id, title_image, user_id, text) VALUES (?, ?, ?, ?)',
+        [groupId, titleImage, userId, text],
+        (insertErr) => {
+          connection.release();
+
+          if (insertErr) {
+            console.error('Error creating post:', insertErr);
+            return res
+              .status(500)
+              .json({ message: 'Error while creating post' });
+          }
+
+          return res.status(200).json({ message: 'Post created' });
+        }
+      );
+    });
+  });
+};
+
 exports.getEventsBySubgroupId = (req, res) => {
   const { subgroupId } = req.params;
 
@@ -118,7 +177,7 @@ exports.createSubroup = (req, res) => {
       }
 
       connection.query(
-        'INSERT INTO subgroups (name, main_group_id, caption, Description, title_image) VALUES (?, ?, ?, ?)',
+        'INSERT INTO subgroups (name, main_group_id, caption, Description, title_image) VALUES (?, ?, ?, ?, ?)',
         [name, mainGroupId, caption, introduction, subgroupImage],
         (insertErr) => {
           connection.release();
@@ -134,5 +193,43 @@ exports.createSubroup = (req, res) => {
         }
       );
     });
+  });
+};
+
+// eslint-disable-next-line consistent-return
+exports.addMemberToSubgroup = (req, res) => {
+  // Retrieve user registration data from the request body
+  const { groupId, userId } = req.body;
+
+  // Validate required fields
+  if (!groupId) {
+    return res.status(400).json({ message: 'Group ID not provided' });
+  }
+  if (!userId) {
+    return res.status(400).json({ message: 'User ID not provided' });
+  }
+
+  // Connect to the database pool
+  // eslint-disable-next-line consistent-return
+  pool.getConnection((dbErr, connection) => {
+    if (dbErr) {
+      console.error('Error getting MySQL connection:', dbErr);
+      return res.status(500).json({ message: 'Failed to connect to MySQL' });
+    }
+
+    connection.query(
+      'UPDATE subgroups SET members = CONCAT(members, ?) WHERE group_id = ?',
+      [userId, groupId],
+      (insertErr) => {
+        connection.release();
+
+        if (insertErr) {
+          console.error('Error adding member:', insertErr);
+          return res.status(500).json({ message: 'Error while adding member' });
+        }
+
+        return res.status(200).json({ message: 'Member added' });
+      }
+    );
   });
 };
--- src/routes/routes/subgroup.js
+++ src/routes/routes/subgroup.js
@@ -6,8 +6,8 @@ const subgroupController = require('../controllers/subgroupController');
 
 router.get('/:subgroupId/posts', subgroupController.getPostsBySubgroupId);
 router.get('/:subgroupId/events', subgroupController.getEventsBySubgroupId);
-// router.post('/', subgroupController.createSubgroup);
-// router.post('/posts', subgroupController.createPost);
+router.post('/', subgroupController.createSubgroup);
+router.post('/posts', subgroupController.createPost);
 // router.post('/events', subgroupController.createEvent);
 
 module.exports = router;
