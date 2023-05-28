c9402fee0a2272ba614b466723a1825f81ac94af
cc201013 <bedo.faruk13@gmail.com>
2023-05-28 11:50:59 +0200

    loading spinner added

--- src/views/Login_Registration/Login/LoginScreen.js
+++ src/views/Login_Registration/Login/LoginScreen.js
@@ -2,26 +2,39 @@ import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
+import { View, StyleSheet } from 'react-native';
+import { ActivityIndicator } from 'react-native-paper';
+import { theme } from '../../../constants/myTheme';
+
 import LoginView from './LoginView';
 import {
   setCurrentUser,
   IpAddress,
 } from '../../../redux/features/mainSlice/mainSlice';
 
-// TODO Validation / Authentication for available User inside DB
-/**
- * This is the main representation of the Login Screen for User to login in their account
- */
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    zIndex: 10,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
 export default function LoginScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [emailError, setEmailError] = useState('');
   const [passwordError, setPasswordError] = useState('');
+  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
   const clientIpAddress = useSelector(IpAddress);
-
-  // navigate to REGISTRATION Screen
   const navigation = useNavigation();
+
   const handleTextClick = () => {
     navigation.navigate('RegistrationOne');
   };
@@ -30,6 +43,8 @@ export default function LoginScreen() {
     event.preventDefault();
 
     try {
+      setLoading(true);
+
       const response = await axios.post(
         `http://${clientIpAddress}:3001/auth/login`,
         {
@@ -39,10 +54,7 @@ export default function LoginScreen() {
       );
 
       const { message, user } = response.data;
-      console.log(message); // Optional: Display success message
-
-      // TODO: Store user data or token in the app state or local storage
-      // Redirect to the main screen or any other screen in your app
+      console.log(message);
       console.log(user);
       navigation.navigate('MainScreen');
       dispatch(setCurrentUser(user));
@@ -60,21 +72,28 @@ export default function LoginScreen() {
       } else {
         // Handle other login errors here
       }
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <LoginView
-      emailError={emailError}
-      emailValue={email}
-      onChangeTextEmail={(value) => setEmail(value)}
-      passwordError={passwordError}
-      passwordValue={password}
-      onChangeTextPassword={(value) => setPassword(value)}
-      handleSubmit={handleSubmit}
-      onNavigateText={handleTextClick}
-
-      // onForgotPassword={handleForgotPassword}
-    />
+    <View style={styles.container}>
+      {loading && (
+        <View style={styles.overlay}>
+          <ActivityIndicator animating color={theme.colors.primary} />
+        </View>
+      )}
+      <LoginView
+        emailError={emailError}
+        emailValue={email}
+        onChangeTextEmail={(value) => setEmail(value)}
+        passwordError={passwordError}
+        passwordValue={password}
+        onChangeTextPassword={(value) => setPassword(value)}
+        handleSubmit={handleSubmit}
+        onNavigateText={handleTextClick}
+      />
+    </View>
   );
 }
