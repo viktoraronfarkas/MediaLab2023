fe5c6485512f7bfe7823311f930ed30212d221db
Paul <paul.hager@tlsoft.at>
2023-06-02 15:19:23 +0200

    working on zooming on inputfield

--- src/views/Login_Registration/Registration/RegistrationPageOneView.js
+++ src/views/Login_Registration/Registration/RegistrationPageOneView.js
@@ -6,6 +6,8 @@ import {
   ScrollView,
   // Image,
   View,
+  KeyboardAvoidingView,
+  Platform,
   // TouchableOpacity,
 } from 'react-native';
 import ClickableText from '../../../components/ClickableText';
@@ -101,7 +103,12 @@ export default function RegistrationPageOneView({
 
   return (
     <SafeAreaView style={style.container}>
-      <ScrollView ref={scrollViewRef} contentContainerStyle={{ flexGrow: 1 }}>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0} // Adjust the offset if needed
+      >
+        <ScrollView ref={scrollViewRef} contentContainerStyle={{ flexGrow: 1 }}>
         <View style={{ paddingHorizontal: 25 }}>
           <View style={{ paddingTop: 30 }}>
             <CaptionScribbleHeading
@@ -223,6 +230,7 @@ export default function RegistrationPageOneView({
           />
         </View>
       </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
