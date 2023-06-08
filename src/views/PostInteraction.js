1e71fe555ec3c98b15b88f18dfef28d6d50590cb
Paul <paul.hager@tlsoft.at>
2023-06-08 15:29:01 +0200

    PostInteraction

--- src/views/PostInteraction.js
+++ src/views/PostInteraction.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { View, Text, ImageBackground, Image } from 'react-native';
+import { View, Text, Image, ScrollView, } from 'react-native';
 import { useSelector } from 'react-redux';
 import { styles, theme } from '../constants/myTheme';
-import BackButton from '../components/Buttons/BackButton';
-// import OrangeButton from '../components/Buttons/OrangeButton';
+import OrangeButton from '../components/Buttons/OrangeButton';
 import underlineImage from '../../assets/Images/thin-underline-image.png';
-import circleLineImage from '../../assets/Images/circleLine-image.png';
-import UnderlineImageSmall from '../../assets/Images/under-line-image.png';
 import { selectedPost } from '../redux/features/mainSlice/mainSlice';
-import BottomScrollSheet from '../components/BottomScrollSheet/BottomScrollSheet';
 
 function PostInteraction() {
   const postData = useSelector(selectedPost);
@@ -19,121 +15,83 @@ function PostInteraction() {
     <SafeAreaView
       style={{ backgroundColor: theme.colors.backgroundSand, flex: 1 }}
     >
-      {/* <BottomScrollSheet
-          bottomSheetRef={refRBSheet}
-          contentComponent={
-            <OptionsLeaveGroupSheet
-              sheetTitle={`Delete Post?`}
-              leaveText="Yes"
-              cancelText="Nevermind"
-              onCancel={() => {
-                refRBSheet.current.close(); // Close the bottom sheet
-              }}
-              onLeave={() => {
-                unsubscribeFromSubGroup();
-              }}
-            />
-          }
-        /> */}
-      <View style={{ margin: 20 }}>
-        <View style={{ alignItems: 'center', marginTop: 20 }}>
-          <Text style={[styles.headline1, { textAlign: 'center' }]}>
-            {postData.title}
-          </Text>
-          <Image source={underlineImage} style={{ width: 340, height: 30 }} />
-        </View>
-
-        <View
-          style={{
-            flex: 1,
-            flexDirection: 'row',
-            justifyContent: 'center',
-            marginTop: 20,
-          }}
-        >
-          <View
-            style={{
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginRight: 80,
-            }}
-          >
-            <Text style={[styles.headline3]}>DisplayName</Text>
-            {/* <Text style={[styles.headline2]}>{postData.subTitle}</Text> */}
-            <Image
-              source={UnderlineImageSmall}
-              style={{
-                width: 120,
-                height: 8,
-              }}
-            />
+      <ScrollView>
+        <View style={{ margin: 20 }}>
+          <View style={{ alignItems: 'center', marginTop: 20 }}>
+            <Text style={[styles.headline1, { textAlign: 'center' }]}>
+              {postData.title}
+            </Text>
+            <Image source={underlineImage} style={{ width: 340, height: 30 }} />
           </View>
-          <ImageBackground
-            source={circleLineImage}
-            style={{
-              width: 120,
-              height: 40,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <Text style={[styles.headline3]}>15:30</Text>
-          </ImageBackground>
-        </View>
-        <Text style={[styles.bodyDefault, { marginTop: 50, paddingHorizontal: 20 }]}>
-          {postData.content}
-        </Text>
+          <Text style={[styles.bodyDefault, { marginTop: 50, paddingHorizontal: 20 }]}>
+            {postData.content}
+          </Text>
 
-        {/* <OrangeButton text="Join Event" style={{ marginTop: 20 }} /> */}
 
-        {/* <View style={{ flexDirection: 'row', marginTop: 80 }}>
-          <Text style={[styles.headline3]}>Organised by:</Text>
-          <View
-            style={{
-              flex: 1,
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
+          <View style={{ flexDirection: 'row', marginTop: 80, justifyContent: 'center', }}>
+            <Text style={[styles.headline3]}>created by:</Text>
             <View
               style={{
-                width: 50,
-                height: 50,
-                borderRadius: 50 / 2,
-                overflow: 'hidden',
-                marginLeft: 5,
-                marginTop: -10,
+                flex: 1,
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
               }}
             >
-              <Image
-                source={Foodshare}
-                style={{ flex: 1, width: null, height: null }}
-                resizeMode="cover"
+              <View
+                style={{
+                  width: 50,
+                  height: 50,
+                  borderRadius: 50 / 2,
+                  overflow: 'hidden',
+                  marginLeft: 5,
+                  marginTop: -10,
+                }}
               />
+              <Text
+                style={[styles.subtitle1, { marginLeft: 10, marginBottom: 5 }]}
+              >
+                Username
+              </Text>
             </View>
-            <Text
-              style={[styles.subtitle1, { marginLeft: 10, marginBottom: 5 }]}
-            >
-              Username
-            </Text>
-          </View>
-        </View> */}
+          </View> 
+          <View style={{ marginTop: 30}}>
+            <View>
+              <Text style={[styles.subtitle1, { marginLeft: 10, marginBottom: 5, color: theme.colors.primary }]}>
+                Displayed Name
+              </Text>
+              <Text style={[styles.bodyDefault, { marginLeft: 10, marginBottom: 20 }]}>
+                Name
+              </Text>
+            </View>
 
-        {/* <View style={{ flexDirection: 'row', marginTop: 30 }}>
-          <Text style={[styles.headline3]}>People joining:</Text>
-          <View
-            style={{
-              flex: 1,
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <Text style={[styles.subtitle1, { marginLeft: 10 }]}>33</Text>
+            <View>
+              <Text  style={[styles.subtitle1, { marginLeft: 10, marginBottom: 5, color: theme.colors.primary }]}>
+                Full Name
+              </Text>
+              <Text style={[styles.bodyDefault, { marginLeft: 10, marginBottom: 20 }]}>
+                Name
+              </Text>
+            </View>
+
+
+            <View>
+              <Text  style={[styles.subtitle1, { marginLeft: 10, marginBottom: 5, color: theme.colors.primary }]}>
+                Email
+              </Text>
+              <Text style={[styles.bodyDefault, { marginLeft: 10, marginBottom: 20 }]}>
+                test.test@fhstp.ac.at
+              </Text>
+            </View>
           </View>
-        </View> */}
-      </View>
+          <View style={{ marginTop: 50 }}>
+          <OrangeButton
+              text="Delete"
+              styleButton={{ alignSelf: 'center', width: '30%' }}
+            />
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
