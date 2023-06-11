3e48a3c850bd1d23e45a1bb7e21ca4d46e36a035
Lenzia207 <lenzia207@gmail.com>
2023-06-11 12:58:34 +0200

    apply image edit ( not working yet)

--- src/views/UserProfile/UserProfile/UserProfileScreen.js
+++ src/views/UserProfile/UserProfile/UserProfileScreen.js
@@ -3,6 +3,7 @@ import { useNavigation } from '@react-navigation/native';
 import * as ImagePicker from 'expo-image-picker';
 import { useDispatch, useSelector } from 'react-redux';
 import UserProfileView from './UserProfileView';
+import useUpdateUserData from '../../../routes/hooks/useUpdateUserData';
 
 import {
   setCurrentUser,
@@ -20,25 +21,19 @@ import useFetchUserData from '../../../routes/hooks/useFetchUserData';
 export default function UserProfileScreen() {
   const currentUser = useSelector(selectedUser);
   const currentUserId = useSelector(selectedUserId);
-  const { userData, studyCourse, imageUpload } =
-    useFetchUserData(currentUserId);
+  const { userData, imageUpload } = useFetchUserData(currentUserId);
   const dispatch = useDispatch();
 
-  const [imageUploaded, setImage] = useState(null);
+  const [imageUploaded, setImage] = useState(imageUpload);
   const [dialogVisible, setDialogVisible] = useState(false);
   const navigation = useNavigation();
-  // const userId = useFetchUserData();
 
-  // // Update imageUploaded when imageUpload changes
   useEffect(() => {
     setImage(imageUpload);
-  }, [imageUpload]);
-
-  useEffect(() => {
     dispatch(setCurrentUser(userData));
-    dispatch(setCurrentUser(studyCourse));
-  }, [dispatch, userData]);
-  // const { updateUser } = useUpdateUserData();
+    dispatch(setCurrentUser(imageUpload));
+  }, [dispatch, userData, imageUpload]);
+  const { updateUser } = useUpdateUserData();
 
   // Open Action Dialog to edit, delete profile image
   const handleDialogOpen = () => {
@@ -46,9 +41,27 @@ export default function UserProfileScreen() {
   };
 
   // Delete Profile Picture (will show default image)
-  const handleDeletePicture = () => {
-    // setImage(null);
-    setDialogVisible(false);
+  const handleDeletePicture = async (event) => {
+    event.preventDefault();
+    const updatedData = {
+      profileImage: null,
+      email: currentUser.email,
+      username: currentUser.username,
+      name: currentUser.name,
+      biography: currentUser.biography,
+    };
+
+    try {
+      await updateUser(updatedData);
+      setImage(null);
+      dispatch(setCurrentUser(updatedData));
+      setDialogVisible(false);
+    } catch (error) {
+      // Handle error if necessary
+      console.error('Error updating user data:', error);
+      throw error;
+    }
+    console.log(updatedData);
   };
   // Cancel the Dialog Action
   const handleCancelDialog = () => {
@@ -60,28 +73,35 @@ export default function UserProfileScreen() {
     const result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
-      includeBase64: false,
+      includeBase64: true,
+      base64: true,
       aspect: [4, 3],
       quality: 1,
     });
 
     if (!result.canceled) {
-      // setImage(result.assets[0].uri);
+      setImage(result.assets[0].uri);
       setDialogVisible(false);
     }
 
-    // const updatedData = {
-    //   profileImage: imageUpload || currentUser.profileImage,
-    // };
+    const updatedData = {
+      email: currentUser.email,
+      username: currentUser.username,
+      name: currentUser.name,
+      biography: currentUser.biography,
+      profileImage: result.assets[0].uri || imageUpload,
+    };
 
-    // try {
-    //   await updateUser(updatedData);
-    //   dispatch(setCurrentUser(updatedData));
-    // } catch (error) {
-    //   // Handle error if necessary
-    //   console.error('Error updating user data:', error);
-    //   throw error;
-    // }
+    try {
+      await updateUser(updatedData);
+      setImage(result.assets[0].uri);
+      dispatch(setCurrentUser(updatedData));
+    } catch (error) {
+      // Handle error if necessary
+      console.error('Error updating user data:', error);
+      throw error;
+    }
+    console.log(updatedData);
   };
 
   return (
